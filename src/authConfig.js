49e101da8dd3a537b7ee6ca67e1758f733245ca5
Seshu1148 <seshachalam.katuri@bluehublabs.net>
2024-04-27 12:54:42 +0530

    Updated API

--- src/authConfig.js
+++ src/authConfig.js
@@ -87,7 +87,7 @@ export const msalConfig = {
  * https://docs.microsoft.com/en-us/azure/active-directory/develop/v2-permissions-and-consent#openid-connect-scopes
  */
 export const loginRequest = {
-    scopes: ["openid", "profile", "https://aipb2cpoc.onmicrosoft.com/mtrest/User.Invite", "https://aipb2cpoc.onmicrosoft.com/mtrest/User.ReadAll"]
+    scopes: ["openid", "profile", "https://alt360.onmicrosoft.com/mtrest/Users.Invite", "https://alt360.onmicrosoft.com/mtrest/Users.ReadAll"]
 };
 
 /**
@@ -95,5 +95,5 @@ export const loginRequest = {
  * between applications by providing a "login_hint" property.
  */
 export const silentRequest = {
-  scopes: ["openid", "profile", "https://aipb2cpoc.onmicrosoft.com/mtrest/User.Invite", "https://aipb2cpoc.onmicrosoft.com/mtrest/User.ReadAll"]
+  scopes: ["openid", "profile", "https://alt360.onmicrosoft.com/mtrest/User.Invite", "https://alt360.onmicrosoft.com/mtrest/User.ReadAll"]
 };
