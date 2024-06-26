a4d6410c20317dd7bf9a0435d8a827747d31188c
Seshu1148 <129722130+Seshu1148@users.noreply.github.com>
2024-06-26 15:50:41 +0530

    Update variables.js

--- src/variables.js
+++ src/variables.js
@@ -2,7 +2,7 @@ export default global = {
     window : {
        variables : {
          //  apiBaseUri: "https://alt360qaapi.azurewebsites.net", // QA alt360api
-          apiBaseUri: "https://alt360devapi.azurewebsites.net", // Dev alt360api
+          apiBaseUri: "https://alt360qaapi.azurewebsites.net", // Dev alt360api
          //  apiBaseUri: "https://localhost:8080",  // use this for the local debugging
           tenantID: "687d523f-0304-4f59-8195-d25a5d0578f4",
           imagePath: "https://aip8c98.blob.core.windows.net/images/"
